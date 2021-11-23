0ddebf0e2c6650fbc996e2c18d5448ac1d55d768
medronas <medron@gmx.net>
2021-11-23 19:55:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 WA.room.onLeaveZone('trigger', () => {
-  
+    WA.chat.sendChatMessage("Goodbye!", 'Mr Robot'),
 	WA.room.showLayer('roof'); 
-});
+})
 
 WA.room.onEnterZone('trigger', () => {
-   
+   WA.chat.sendChatMessage("Welcome!", 'Mr Robot'),
 		WA.room.hideLayer('roof');
-});
+})
 
 
 WA.room.onLeaveZone('party', () => {
-    
+    WA.chat.sendChatMessage("Goodbye!", 'Mr Robot'),
 	WA.room.showLayer('partyroof'); 
-});
+})
 
 WA.room.onEnterZone('party', () => {
-   
+   WA.chat.sendChatMessage("Welcome!", 'Mr Robot'),
 		WA.room.hideLayer('partyroof');
 })
